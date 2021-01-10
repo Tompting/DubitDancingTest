f50bb5d711907faa0bd3da6fc11e43293eb27032
tommackintosh <tom.mackintosh2001@live.co.uk>
2021-01-10 16:19:16 +0000

    Uploaded to repository.
    Initial commit

---
+++ ExampleSong.lua
@@ -0,0 +1,27 @@
+-- Example Song
+-- Tom Mackintosh
+-- 10th January 2021
+
+local module = {
+	1,
+	1,
+	0.5,
+	0.25,
+	0.25,
+	1,
+	2,
+	0.5,
+	0.5,
+	1,
+	1,
+	1,
+	1,
+	0.5,
+	0.5,
+	1,
+	0.5,
+	1,
+	
+}
+
+return module
